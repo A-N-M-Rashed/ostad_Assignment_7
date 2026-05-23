863c22a2a4b022e08b9959dabfde536a69916554
A-N-M-Rashed <a.n.m.rashed045@gmail.com>
2026-05-23 14:01:14 +0600

    Task 2: Calculator Using Operator

--- script.js
+++ script.js
@@ -10,3 +10,15 @@ document.getElementById("courseName").innerHTML= courseName;
 document.getElementById("isEnrolled").innerHTML= isEnrolled;
 //Student Information Ends
 
+//Calculator
+var num1=10;
+var num2=15;
+var add=num1+num2;
+var sub=num2-num1;
+var multi=num1*num2;
+var division=num2/num1;
+
+document.querySelector("#add").innerHTML= add;
+document.querySelector("#sub").innerHTML= sub;
+document.querySelector("#multi").innerHTML= multi;
+document.querySelector("#division").innerHTML= division;
