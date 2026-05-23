2d223b759295708528a5fa3dec92cedff0b31dcd
A-N-M-Rashed <a.n.m.rashed045@gmail.com>
2026-05-23 12:25:39 +0600

    student information added

---
+++ README.md
@@ -0,0 +1,1 @@
+"# ostad_Assignment_7" 
---
+++ script.js
@@ -0,0 +1,12 @@
+//Student Information
+var studentName= "Robin";
+var age="20";
+var courseName= "Sociology";
+var isEnrolled= true;
+
+document.getElementById("sName").innerHTML= studentName;
+document.getElementById("age").innerHTML= age;
+document.getElementById("courseName").innerHTML= courseName;
+document.getElementById("isEnrolled").innerHTML= isEnrolled;
+//Student Information Ends
+
